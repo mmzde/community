c3cc7a0e0195d7a1b850f46b0c751cd21eafc934
communityru <892884792@qq.com>
2020-07-20 22:45:44 +0800

    session持久化

--- src/main/java/sky/starry/community/controller/helloController.java
+++ src/main/java/sky/starry/community/controller/helloController.java
@@ -1,17 +1,42 @@
 package sky.starry.community.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import sky.starry.community.mapper.UserMapper;
+import sky.starry.community.model.User;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class helloController {
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping("/")
-    public String Hello(Model model){
+    public String Hello(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null&&cookies.length>0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+
         return "index";
     }
     @RequestMapping("/hello")
-    public String Hello(@RequestParam(name = "name",defaultValue = "姓名七个字") String name, Model model){
+    public String Hello(@RequestParam(name = "name",defaultValue = "姓名七个字") String name,
+                        Model model){
         model.addAttribute("name",name);
         return "hello";
     }
--- src/main/java/sky/starry/community/mapper/UserMapper.java
+++ src/main/java/sky/starry/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package sky.starry.community.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import sky.starry.community.model.User;
 
 @Mapper
@@ -11,4 +13,6 @@ public interface UserMapper{
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{account_id},#{token},#{gmt_create},#{gmt_modified})")
     void insert(User user);
 
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
